3d9d9438cacc8f88c7b0c22a2295bfc1fb91fdb3
Felton Vaughn <felton@users.noreply.github.com>
2018-10-12 15:41:35 -0400

    BWSR-368: Initial structure

---
+++ test/codeception/unit/src/Test.php
@@ -0,0 +1,16 @@
+<?php
+class Test extends \Codeception\Test\Unit
+{
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+    }
+}
